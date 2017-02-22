73f7e1e6a82ff89f68da50c0d0009885de8b461e
Ron Wehrens <ron.wehrens@wur.nl>
2017-02-22 10:41:40 +0100

    fixed bug in evaluateCorrection function

--- R/evaluateCorrection.R
+++ R/evaluateCorrection.R
@@ -1,6 +1,6 @@
 evaluateCorrection <- function(X, Y, what = c("duplo", "PCA"), ...) {
   what <- match.arg(what)
   switch(what,
-         duplo = evaluateDuplos(X, Y, ..., plot = FALSE, perMetabolite = FALSE),
-         evaluatePCA(X, Y, ..., plot = FALSE, perBatch = FALSE))
+         duplo = evaluateDuplos(X, Y, ..., perMetabolite = FALSE),
+         evaluatePCA(X, Y, ..., perBatch = FALSE))
 }
